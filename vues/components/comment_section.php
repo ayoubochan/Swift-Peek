27bcb7a7767fd5f5e1172ebc7018388242466a74
Adam_Loukili <ad.loukili@outlook.fr>
2019-10-23 09:24:51 +0200

    Comments_done

---
+++ vues/components/comment_section.php
@@ -0,0 +1,7 @@
+<section class="ajoutcomms"> 
+  <form method="post" >
+  <p><label>Votre Pseudo : </label><input type="text" id="addpseudo" name="addpseudo"></p>
+  <p><label>Votre Commentaire : </label><input type="text" id="addcomment" name="addcomment" placeholder="votre commentaire ici ..."></p>
+  <p><input type="submit" name="sendcomment" value="Envoyer votre commentaire" /></p>
+  </form>
+  </section>
