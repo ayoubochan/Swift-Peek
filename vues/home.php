518ced2ddc9dc60cb14b911edaff1c26b6f99609
a-o013 <ayoub-ochan@hotmail.com>
2019-10-29 22:18:55 +0100

    test

--- vues/home.php
+++ vues/home.php
@@ -20,52 +20,79 @@
 <script>
   let backdrop = ''
   let limit = 0
+  let saveLimit = 1
+  let stock = []
   let page = 1
-  let movies = []
   const list = document.querySelector('#movieList')
 
-  list.onscroll = (e) => {
-    if((list.scrollTop + list.offsetHeight) >= (list.scrollHeight - 500)) {
-      page ++
-      getMovies(page)
+//   function get() {
+//     for(let i = 1; i <= 20; i++) {
+//       fetch(`https://api.themoviedb.org/3/discover/movie?api_key=b53ba6ff46235039543d199b7fdebd90&language=en-US&page=${i}`)
+//       .then(response  =>  response.json())
+//       .then(data  => {
+//         movies = movies.concat(data.results)
+//         if(i % 10 === 0) show()
+//     })
+//   }
+// }
+
+// window.onload = () => {get()}
+
+//   function show() {
+//     if(movies[0] === undefined) {
+//       document.location.reload()
+//     } else {
+//       console.log(movies)
+//     }
+//   }
+
+  list.onscroll = () => {
+    if((list.scrollTop + list.offsetHeight) >= (list.scrollHeight - 100)) {
+      limit += 20
+      showMovies()
     }
   }
 
-  function getMovies(page) {
-    
-    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=b53ba6ff46235039543d199b7fdebd90&language=en-US&page=${page}`)
-    .then(response  =>  response.json())
-    .then(data  => {
-      movies = movies.concat(data.results)
-      showMovies(movies)
+  function getMovies() {
+    for(let i = 1; i <= 20; i++) {
+      fetch(`https://api.themoviedb.org/3/discover/movie?api_key=b53ba6ff46235039543d199b7fdebd90&language=en-US&page=${i}`)
+      .then(response  =>  response.json())
+      .then(data  => {
+        stock = stock.concat(data.results)
+        if(i % 10 === 0) showMovies(stock)
     })
+    }
   }
   getMovies()
 
   function showMovies(movies) {
-    console.log(movies)
-    const movieList = movies.slice(limit).map((elem, index) => {
-      if(elem.poster_path !== null) {
-        const img = document.createElement('img')
-        const li = document.createElement('li')
-        list.appendChild(li)
-        li.appendChild(img)
-        img.src = `https://image.tmdb.org/t/p/w200/${elem.poster_path}`
-        backdrop = elem.backdrop_path
+    if(movies[0] === undefined) {
+      document.location.reload()
+    } else {
+      if(limit !== saveLimit) {
+      saveLimit = limit
+      movies.slice(limit, limit + 20).map((elem, index) => {
+        if(elem.poster_path !== null) {
+          const img = document.createElement('img')
+          const li = document.createElement('li')
+          list.appendChild(li)
+          li.appendChild(img)
+          img.src = `https://image.tmdb.org/t/p/w200/${elem.poster_path}`
 
-        li.onclick = () => {
-          var xmlhttp = new XMLHttpRequest();
-          xmlhttp.onreadystatechange = function() {
-              if (this.readyState == 4 && this.status == 200) {
-                window.location.href = `${window.location}?i=${elem.id}`
-              }
-          };
-          xmlhttp.open("GET", "index.php?i=" + elem.id, true);
-          xmlhttp.send();
+          li.onclick = () => {
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                  window.location.href = `${window.location}?i=${elem.id}`
+                }
+            };
+            xmlhttp.open("GET", "index.php?i=" + elem.id, true);
+            xmlhttp.send();
+          }
         }
-      }
-    })
-    limit += 20
+      })
+    }
+    }
   }
   
   </script>
