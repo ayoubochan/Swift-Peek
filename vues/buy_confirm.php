22cf93ebeb38e441ee56d0eec728c17838118c78
ShaoYuan <shaoyuan.weng@gmail.com>
2019-11-08 14:18:51 +0100

    variable name change

--- vues/buy_confirm.php
+++ vues/buy_confirm.php
@@ -1,5 +1,6 @@
 <?php
-//session_start(); 
+session_start(); 
+$codePromo = "MikeEstTropCool";
 if (isset($_POST['nom'])) {
     $_SESSION['form_buy']['nom']=$_POST['nom'];
 }
@@ -17,7 +18,7 @@ if (isset($_POST['checkout'])) {
 } else {
     $quantite = 0;
 }
-print_r($_SESSION);
+// print_r ($_SESSION[]);
 ?>
 
 <!DOCTYPE html>
@@ -32,29 +33,30 @@ print_r($_SESSION);
 </head>
 <body onload="calculFraisTotal()">
     <div class="container">
-        <form method="POST" action="vues/buy_confirm.php">
+        <!-- Formulaire de commande -->
+        <form method="POST" action="" onsubmit="msgConfirmation()">
             
             <label for="nbreFilms">Nombre de films</label>
             <input class="form-control" id="nbreFilms" type="text" value="<?php echo $quantite; ?>" onchange="calculFraisTotal()" disabled/>
             <label for="reduction">Réduction (%)</label>
             <input class="form-control" id="reduction" type="text" disabled/>
-            <label for="nom">Prénom - Nom</label>
+            <label for="nom">Prénom - Nom *</label>
             <?php if (!empty($_SESSION['form_buy']['nom']))
-                        echo '<input class="form-control" id="nom" name="nom" type="text" value="'.$_SESSION['form_buy']['nom'].'" />';
+                        echo '<input class="form-control" id="nom" name="nom" type="text" value="'.$_SESSION['form_buy']['nom'].'" required/>';
                     else 
-                        echo '<input class="form-control" id="nom" name="nom" type="text" />';
+                        echo '<input class="form-control" id="nom" name="nom" type="text" required/>';
             ?>
-            <label for="rue">Rue</label>
+            <label for="rue">Rue *</label>
             <?php if (!empty($_SESSION['form_buy']['rue']))
-                        echo '<input class="form-control" id="rue" name="rue" type="text" value="'.$_SESSION['form_buy']['rue'].'" />';
+                        echo '<input class="form-control" id="rue" name="rue" type="text" value="'.$_SESSION['form_buy']['rue'].'" required/>';
                     else 
-                        echo '<input class="form-control" id="rue" name="rue" type="text" />';
+                        echo '<input class="form-control" id="rue" name="rue" type="text" required/>';
             ?>
-            <label for="ville">Ville</label>
+            <label for="ville">Ville *</label>
             <?php if (!empty($_SESSION['form_buy']['ville']))
-                        echo '<input class="form-control" id="ville" name="ville" type="text" value="'.$_SESSION['form_buy']['ville'].'" />';
+                        echo '<input class="form-control" id="ville" name="ville" type="text" value="'.$_SESSION['form_buy']['ville'].'" required/>';
                     else 
-                        echo '<input class="form-control" id="ville" name="ville" type="text" />';
+                        echo '<input class="form-control" id="ville" name="ville" type="text" required/>';
             ?>
             <label for="pays">Pays</label>
             <select class="form-control" id="pays" name="pays"onchange="calculFraisTotal()" >
@@ -72,23 +74,35 @@ print_r($_SESSION);
             <label for="promo">Promo (%)</label>
             <input class="form-control" id="promo" type="text" disabled/> 
             <label for="total">Total à payer (Eur)</label>
-            <input class="form-control" id="total" type="text" /><br> 
+            <input class="form-control" id="total" type="text" /><br>
+            <a>Les champs indiqués par une * sont obligatoires</a><br> 
 
             <button type="submit" class="btn btn-primary">Commander</button>
         </form>
     <div>
     <script type="text/javascript">
+        // Rend la touche Enter inactive
         window.addEventListener('keydown',function(e){if(e.keyIdentifier=='U+000A'||e.keyIdentifier=='Enter'||e.keyCode==13){if(e.target.nodeName=='INPUT'&&e.target.type=='text'){e.preventDefault();return false;}}},true)
+            
+        // Message de confirmation de la commande
+        function msgConfirmation() {
+            alert("Commande envoyée");
+        }
+         
+        // Calcul le montant total avec les frais et les réductions
         function calculFraisTotal() {
             var prix = 10; // Prix unique pour les articles en Eur
             var codePro = document.getElementById("codePromo").value;
-            var codePromo = "MikeEstTropCool";
+            
+            // Vérification du code promo
+            //var codePromo = "MikeEstTropCool";
+            var codePromo = <?php echo json_encode($codePromo); ?>;
             var codePromoOk = false;
-
             if (codePro == codePromo) {
                 codePromoOk = true;
             }
             
+            // Calcul de la réduction
             var quantite = document.getElementById("nbreFilms").value;
             var reduction = 0;
 
@@ -99,6 +113,7 @@ print_r($_SESSION);
             }
             document.getElementById("reduction").value = reduction;
             
+            // Calcul des frais et de la promo
             var sel = document.getElementById("pays");
             var pays=sel.options[sel.selectedIndex].value;
             var livraison = 0;
@@ -145,6 +160,7 @@ print_r($_SESSION);
                     break;
                 default:
             }
+            // Calcul du total à payer
             var total = 0;
             var totalReduction = 0;
             total = quantite * prix;
@@ -154,7 +170,6 @@ print_r($_SESSION);
             } else {
                 document.getElementById("total").value = 0;
             }
-
         }
   </script>
     <!-- Bootstrap core JavaScript -->
--- vues/old_buy_confirm.php
+++ vues/old_buy_confirm.php
@@ -1,6 +1,5 @@
 <?php
-session_start(); 
-$codePromo = "MikeEstTropCool";
+//session_start(); 
 if (isset($_POST['nom'])) {
     $_SESSION['form_buy']['nom']=$_POST['nom'];
 }
@@ -13,12 +12,12 @@ if (isset($_POST['ville'])) {
 if (isset($_POST['pays'])) {
     $_SESSION['form_buy']['pays']=$_POST['pays'];
 }
-if (isset($_COOKIE['quantite'])) {
-    $quantite = $_COOKIE["quantite"];
+if (isset($_POST['checkout'])) {
+    $quantite = $_POST['checkout'];
 } else {
     $quantite = 0;
 }
-// print_r ($_SESSION[]);
+print_r($_SESSION);
 ?>
 
 <!DOCTYPE html>
@@ -33,30 +32,29 @@ if (isset($_COOKIE['quantite'])) {
 </head>
 <body onload="calculFraisTotal()">
     <div class="container">
-        <!-- Formulaire de commande -->
-        <form method="POST" action="" onsubmit="msgConfirmation()">
+        <form method="POST" action="vues/buy_confirm.php">
             
             <label for="nbreFilms">Nombre de films</label>
             <input class="form-control" id="nbreFilms" type="text" value="<?php echo $quantite; ?>" onchange="calculFraisTotal()" disabled/>
             <label for="reduction">Réduction (%)</label>
             <input class="form-control" id="reduction" type="text" disabled/>
-            <label for="nom">Prénom - Nom *</label>
+            <label for="nom">Prénom - Nom</label>
             <?php if (!empty($_SESSION['form_buy']['nom']))
-                        echo '<input class="form-control" id="nom" name="nom" type="text" value="'.$_SESSION['form_buy']['nom'].'" required/>';
+                        echo '<input class="form-control" id="nom" name="nom" type="text" value="'.$_SESSION['form_buy']['nom'].'" />';
                     else 
-                        echo '<input class="form-control" id="nom" name="nom" type="text" required/>';
+                        echo '<input class="form-control" id="nom" name="nom" type="text" />';
             ?>
-            <label for="rue">Rue *</label>
+            <label for="rue">Rue</label>
             <?php if (!empty($_SESSION['form_buy']['rue']))
-                        echo '<input class="form-control" id="rue" name="rue" type="text" value="'.$_SESSION['form_buy']['rue'].'" required/>';
+                        echo '<input class="form-control" id="rue" name="rue" type="text" value="'.$_SESSION['form_buy']['rue'].'" />';
                     else 
-                        echo '<input class="form-control" id="rue" name="rue" type="text" required/>';
+                        echo '<input class="form-control" id="rue" name="rue" type="text" />';
             ?>
-            <label for="ville">Ville *</label>
+            <label for="ville">Ville</label>
             <?php if (!empty($_SESSION['form_buy']['ville']))
-                        echo '<input class="form-control" id="ville" name="ville" type="text" value="'.$_SESSION['form_buy']['ville'].'" required/>';
+                        echo '<input class="form-control" id="ville" name="ville" type="text" value="'.$_SESSION['form_buy']['ville'].'" />';
                     else 
-                        echo '<input class="form-control" id="ville" name="ville" type="text" required/>';
+                        echo '<input class="form-control" id="ville" name="ville" type="text" />';
             ?>
             <label for="pays">Pays</label>
             <select class="form-control" id="pays" name="pays"onchange="calculFraisTotal()" >
@@ -74,35 +72,23 @@ if (isset($_COOKIE['quantite'])) {
             <label for="promo">Promo (%)</label>
             <input class="form-control" id="promo" type="text" disabled/> 
             <label for="total">Total à payer (Eur)</label>
-            <input class="form-control" id="total" type="text" /><br>
-            <a>Les champs indiqués par une * sont obligatoires</a><br> 
+            <input class="form-control" id="total" type="text" /><br> 
 
             <button type="submit" class="btn btn-primary">Commander</button>
         </form>
     <div>
     <script type="text/javascript">
-        // Rend la touche Enter inactive
         window.addEventListener('keydown',function(e){if(e.keyIdentifier=='U+000A'||e.keyIdentifier=='Enter'||e.keyCode==13){if(e.target.nodeName=='INPUT'&&e.target.type=='text'){e.preventDefault();return false;}}},true)
-            
-        // Message de confirmation de la commande
-        function msgConfirmation() {
-            alert("Commande envoyée");
-        }
-         
-        // Calcul le montant total avec les frais et les réductions
         function calculFraisTotal() {
             var prix = 10; // Prix unique pour les articles en Eur
             var codePro = document.getElementById("codePromo").value;
-            
-            // Vérification du code promo
-            //var codePromo = "MikeEstTropCool";
-            var codePromo = <?php echo json_encode($codePromo); ?>;
+            var codePromo = "MikeEstTropCool";
             var codePromoOk = false;
+
             if (codePro == codePromo) {
                 codePromoOk = true;
             }
             
-            // Calcul de la réduction
             var quantite = document.getElementById("nbreFilms").value;
             var reduction = 0;
 
@@ -113,7 +99,6 @@ if (isset($_COOKIE['quantite'])) {
             }
             document.getElementById("reduction").value = reduction;
             
-            // Calcul des frais et de la promo
             var sel = document.getElementById("pays");
             var pays=sel.options[sel.selectedIndex].value;
             var livraison = 0;
@@ -160,7 +145,6 @@ if (isset($_COOKIE['quantite'])) {
                     break;
                 default:
             }
-            // Calcul du total à payer
             var total = 0;
             var totalReduction = 0;
             total = quantite * prix;
@@ -170,6 +154,7 @@ if (isset($_COOKIE['quantite'])) {
             } else {
                 document.getElementById("total").value = 0;
             }
+
         }
   </script>
     <!-- Bootstrap core JavaScript -->
